b56451012dcf2360f1d39230ca4d19ccfbd994ba
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-11-29 23:18:40 -0500

    search is working

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -5,36 +5,50 @@ import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material'
 
 const SearchBar = () => {
-  return (
-    <Paper 
-        component="form"
-        onSubmit={() => {}}
-        sx={{
-            borderRadius: 20,
-            border: "1px solid #e3e3e3",
-            pl: 2,
-            boxShadow: 'none',
-            mr: {
-                sm: 5
-            }
-        }}>
-        <input 
-            className='search-bar'
-            placeholder='Search...'
-            value = ""
-            onChange={() => {}} 
-        />
-        <IconButton 
-            type="submit" 
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchTerm){
+            navigate(`/search/${searchTerm}`)
+
+            setSearchTerm("")
+        }
+    }
+
+    return (
+        <Paper 
+            component="form"
+            onSubmit={handleSubmit}
             sx={{
-                p: "10px",
-                color: "red"
-            }}
-        >
-            <Search />
-        </IconButton>
-            
-    </Paper>
+                borderRadius: 20,
+                border: "1px solid #e3e3e3",
+                pl: 2,
+                boxShadow: 'none',
+                mr: {
+                    sm: 5
+                }
+            }}>
+            <input 
+                className='search-bar'
+                placeholder='Search...'
+                value = {searchTerm}
+                onChange={(e) => {setSearchTerm(e.target.value)}} 
+            />
+            <IconButton 
+                type="submit" 
+                sx={{
+                    p: "10px",
+                    color: "red"
+                }}
+            >
+                <Search />
+            </IconButton>
+                
+        </Paper>
   )
 }
 
