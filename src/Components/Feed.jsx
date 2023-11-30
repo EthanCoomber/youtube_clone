8039a8b0817990d99c26e5f86e4bdfe8782a378c
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-11-29 19:12:01 -0500

    working on VideoCard

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -5,9 +5,11 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
     const [selectedCategory, setSelectedCategory] = useState("New")
+    const [videos, setVideos] = useState([])
 
     useEffect(() => {
         fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+        .then((data) => {setVideos(data.items)})
     }, [selectedCategory]);
 
     return (
@@ -43,7 +45,7 @@ const Feed = () => {
                         Videos
                     </span>
                 </Typography>
-                <Videos videos = {[]}/>
+                <Videos videos={videos}/>
             </Box>
         </Stack>
     )
